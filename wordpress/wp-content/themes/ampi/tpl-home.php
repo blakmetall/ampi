8b35b785b759e81e4ffdc50a0f76d28050d7c36f
Jose Faro <blakmetall@gmail.com>
2020-08-31 22:57:41 -0500

    - integración simple y conexión simple lista

--- wordpress/wp-content/themes/ampi/tpl-home.php
+++ wordpress/wp-content/themes/ampi/tpl-home.php
@@ -18,16 +18,16 @@
             <div class="carousel-caption no-gutters">
                 <div class="col-sm-9 col-md-8 col-lg-7 text-left">
                     <h6
-                        class="text-uppercase font-weight-light mb-2 app-font-21 letter-spacing-6 d-none d-sm-block animate__animated animate__flipInX">
+                        class="text-uppercase font-weight-light mb-2 app-font-30 letter-spacing-6 d-none d-sm-block animate__animated animate__flipInX">
                         Bahía de Banderas
                     </h6>
                     <h5
-                        class="app-font-weight-bolder app-font-36 mb-3 letter-spacing-3 animate__animated animate__flipInX">
+                        class="app-font-weight-bolder app-font-48 mb-3 letter-spacing-3 animate__animated animate__flipInX">
                         Association of Real Estate
                         Professionals
                     </h5>
                     <div class="app-line w-80 warning mb-3 animate__animated animate__flipInX"></div>
-                    <p class="app-font-weight-500 mb-0 d-none d-sm-block animate__animated animate__flipInX">
+                    <p class="app-font-weight-500 app-font-21 mb-0 d-none d-sm-block animate__animated animate__flipInX">
                         Vallarta. Nayarit. Compostela</p>
                 </div>
             </div>
@@ -35,19 +35,20 @@
         <div class="carousel-item">
             <img src="<?=THEME_PATH?>/img/demo-slide-1.jpg" class="d-block w-100" alt="">
             <div class="app-carousel-overlay"></div>
-            <div class="carousel-caption">
+            <div class="carousel-caption no-gutters">
                 <div class="col-sm-9 col-md-8 col-lg-7 text-left">
                     <h6
-                        class="text-uppercase font-weight-light mb-2 app-font-21 letter-spacing-6 d-none d-sm-block animate__animated animate__flipInX">
-                        City or Location
+                        class="text-uppercase font-weight-light mb-2 app-font-30 letter-spacing-6 d-none d-sm-block animate__animated animate__flipInX">
+                        Bahía de Banderas
                     </h6>
                     <h5
-                        class="app-font-weight-bolder app-font-36 mb-3 letter-spacing-3 animate__animated animate__flipInX">
-                        Carousel Title
+                        class="app-font-weight-bolder app-font-51 mb-3 letter-spacing-3 animate__animated animate__flipInX">
+                        Association of Real Estate
+                        Professionals
                     </h5>
                     <div class="app-line w-80 warning mb-3 animate__animated animate__flipInX"></div>
-                    <p class="app-font-weight-500 mb-0 d-none d-sm-block animate__animated animate__flipInX">
-                        Example. Example. Example</p>
+                    <p class="app-font-weight-500 app-font-21 mb-0 d-none d-sm-block animate__animated animate__flipInX">
+                        Vallarta. Nayarit. Compostela</p>
                 </div>
             </div>
         </div>
@@ -60,14 +61,14 @@
 <!-- presentation -->
 <div class="container">
     <!-- title -->
-    <h1 class="mb-4 font-weight-bold app-font-36">Real Estate in Bahía de Banderas</h1>
+    <h1 class="mb-4 font-weight-bold app-font-41">Real Estate in Bahía de Banderas</h1>
     <div class="app-line secondary xxs mb-5"></div>
 
     <div class="card-deck">
         <div class="card border-0 app-card-1">
             <img src="<?=THEME_PATH?>/img/icons/home-icon-1.png" alt="" class="width-auto mb-3">
-            <h2 class="font-weight-bold mb-3 app-font-21">Buying a property</h2>
-            <p class="mb-0 text-secondary">
+            <h2 class="font-weight-bold mb-3 app-font-24">Buying a property</h2>
+            <p class="mb-0 text-secondary app-font-16">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, molestias autem minus iste placeat
                 dolorem hic blanditiis consectetur distinctio magnam totam harum sit, velit ex voluptas ratione?
                 Quasi, eum sunt!
@@ -75,8 +76,8 @@
         </div>
         <div class="card border-0 app-card-1">
             <img src="<?=THEME_PATH?>/img/icons/home-icon-2.png" alt="" class="width-auto mb-3">
-            <h2 class="font-weight-bold mb-3 app-font-21">Buying a property</h2>
-            <p class="mb-0 text-secondary">
+            <h2 class="font-weight-bold mb-3 app-font-24">Buying a property</h2>
+            <p class="mb-0 text-secondary app-font-16">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, molestias autem minus iste placeat
                 dolorem hic blanditiis consectetur distinctio magnam totam harum sit, velit ex voluptas ratione?
                 Quasi, eum sunt!
@@ -84,8 +85,8 @@
         </div>
         <div class="card border-0 app-card-1">
             <img src="<?=THEME_PATH?>/img/icons/home-icon-3.png" alt="" class="width-auto mb-3">
-            <h2 class="font-weight-bold mb-3 app-font-21">Buying a property</h2>
-            <p class="mb-0 text-secondary">
+            <h2 class="font-weight-bold mb-3 app-font-24">Buying a property</h2>
+            <p class="mb-0 text-secondary app-font-16">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, molestias autem minus iste placeat
                 dolorem hic blanditiis consectetur distinctio magnam totam harum sit, velit ex voluptas ratione?
                 Quasi, eum sunt!
@@ -105,9 +106,9 @@
     <div class="pt-5 pb-4"></div>
     <div class="container">
         <!-- title -->
-        <h2 class="mb-4-2 font-weight-bold h1 app-font-36">Search for a Property</h2>
+        <h2 class="mb-4-2 font-weight-bold h1 app-font-41">Search for a Property</h2>
 
-        <form>
+        <form class="app-font-16">
             <!-- row 1 -->
             <div class="row">
                 <div class="col-sm-6 mb-4">
@@ -183,7 +184,7 @@
 <div class="container">
     <!-- news title -->
     <div class="d-flex align-items-center justify-content-between mb-4">
-        <h1 class="mb-0 font-weight-bold app-font-36">Puerto Vallarta & Riviera Nayarit News</h1>
+        <h1 class="mb-0 font-weight-bold app-font-36">Latest News</h1>
         <a href="#" class="d-flex align-items-center ml-3 text-decoration-none position-relative app-top-1">
             <div class="pr-2">View All News</div>
             <i class="fas fa-arrow-right text-warning"></i>
@@ -192,16 +193,13 @@
     <div class="app-line secondary xxs mb-5"></div>
 
 
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 mb-n4">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 mb-n4">
         <div class="col mb-5">
             <div class="card border-0 app-card-2">
                 <a href="#" class="d-inline-block text-decoration-none text-dark">
-                    <img src="<?=THEME_PATH?>/img/demo-blog-1.png" alt="" class="mb-3">
-                    <h2 class="font-weight-500 mb-2 app-font-16 text-dark">Buying a property</h2>
-                    <div class="app-line warning w-20 sm mb-3"></div>
-                    <p class="mb-3 text-dark app-font-14">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, molestias.
-                    </p>
+                    <img src="http://via.placeholder.com/400x300" alt="" class="mb-3">
+                    <div class="app-line warning w-20 sm mb-2"></div>
+                    <h2 class="font-weight-500 mb-3 app-font-16 text-dark">Buying a property</h2>
                     <div class="d-flex align-items-center">
                         <div class="fas fa-clock text-warning mr-2 app-font-13"></div>
                         <span class="app-font-13 text-gray-600">Noviembre, 2020</span>
@@ -212,12 +210,9 @@
         <div class="col mb-5">
             <div class="card border-0 app-card-2">
                 <a href="#" class="d-inline-block text-decoration-none text-dark">
-                    <img src="<?=THEME_PATH?>/img/demo-blog-1.png" alt="" class="mb-3">
-                    <h2 class="font-weight-500 mb-2 app-font-16 text-dark">Buying a property</h2>
-                    <div class="app-line warning w-20 sm mb-3"></div>
-                    <p class="mb-3 text-dark app-font-14">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, molestias.
-                    </p>
+                    <img src="http://via.placeholder.com/400x300" alt="" class="mb-3">
+                    <div class="app-line warning w-20 sm mb-2"></div>
+                    <h2 class="font-weight-500 mb-3 app-font-16 text-dark">Buying a property</h2>
                     <div class="d-flex align-items-center">
                         <div class="fas fa-clock text-warning mr-2 app-font-13"></div>
                         <span class="app-font-13 text-gray-600">Noviembre, 2020</span>
@@ -228,28 +223,9 @@
         <div class="col mb-5">
             <div class="card border-0 app-card-2">
                 <a href="#" class="d-inline-block text-decoration-none text-dark">
-                    <img src="<?=THEME_PATH?>/img/demo-blog-1.png" alt="" class="mb-3">
-                    <h2 class="font-weight-500 mb-2 app-font-16 text-dark">Buying a property</h2>
-                    <div class="app-line warning w-20 sm mb-3"></div>
-                    <p class="mb-3 text-dark app-font-14">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, molestias.
-                    </p>
-                    <div class="d-flex align-items-center">
-                        <div class="fas fa-clock text-warning mr-2 app-font-13"></div>
-                        <span class="app-font-13 text-gray-600">Noviembre, 2020</span>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div class="col mb-5">
-            <div class="card border-0 app-card-2">
-                <a href="#" class="d-inline-block text-decoration-none text-dark">
-                    <img src="<?=THEME_PATH?>/img/demo-blog-1.png" alt="" class="mb-3">
-                    <h2 class="font-weight-500 mb-2 app-font-16 text-dark">Buying a property</h2>
-                    <div class="app-line warning w-20 sm mb-3"></div>
-                    <p class="mb-3 text-dark app-font-14">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, molestias.
-                    </p>
+                    <img src="http://via.placeholder.com/400x300" alt="" class="mb-3">
+                    <div class="app-line warning w-20 sm mb-2"></div>
+                    <h2 class="font-weight-500 mb-3 app-font-16 text-dark">Buying a property</h2>
                     <div class="d-flex align-items-center">
                         <div class="fas fa-clock text-warning mr-2 app-font-13"></div>
                         <span class="app-font-13 text-gray-600">Noviembre, 2020</span>
@@ -268,7 +244,7 @@
     <div class="app-overlay"></div>
     <div class="container position-relative py-4">
         <div class="pt-5 pb-4"></div>
-        <h2 class="mb-5 text-white app-font-33">
+        <h2 class="mb-5 text-white app-font-41">
             <span>Real Estate</span> Professionals you can trust
         </h2>
 
@@ -390,7 +366,6 @@
     </div>
     <div class="pt-5 pb-4"></div>
 </div>
-
 
 <?php get_footer();?>
 <?php include 'includes/modules/footer.php'; ?>
